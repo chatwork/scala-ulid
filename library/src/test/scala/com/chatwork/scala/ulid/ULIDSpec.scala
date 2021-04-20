32a31d5e926732ef1e952ed9f1dd3af0dd48feeb
Junichi Kato <j5ik2o@gmail.com>
2021-04-20 23:56:41 +0900

    build: Add support for sbt-ci-release

--- library/src/test/scala/com/chatwork/scala/ulid/ULIDSpec.scala
+++ library/src/test/scala/com/chatwork/scala/ulid/ULIDSpec.scala
@@ -1,6 +1,6 @@
 package com.chatwork.scala.ulid
 
-import org.scalacheck.{Gen, Shrink}
+import org.scalacheck.{ Gen, Shrink }
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
@@ -34,16 +34,16 @@ class ULIDSpec extends AnyFreeSpec with Matchers with ScalaCheckDrivenPropertyCh
       val ulid3 = ULID.generateMonotonic(ulid2)
       val ulid4 = ULID.generateMonotonic(ulid2, () => ulid2.toEpochMilliAsLong)
 
-      ulid should be < (ulid2)
-      ulid2 should be < (ulid3)
-      ulid3 should be > (ulid2)
-      ulid should be < (ulid3)
+      ulid should be < ulid2
+      ulid2 should be < ulid3
+      ulid3 should be > ulid2
+      ulid should be < ulid3
       ulid.compareTo(ulid) shouldBe 0
       ulid2 should be < ulid4
     }
     "generateStrictlyMonotonic" in forAll(ulidGen) { ulid =>
       val ulid2 = ULID.generateStrictlyMonotonic(ulid).get
-      ulid should be < (ulid2)
+      ulid should be < ulid2
     }
     "toBytes" in forAll(ulidGen) { ulid =>
       val bytes = ulid.toBytes
